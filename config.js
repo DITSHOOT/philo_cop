28f5e38ae433765287803df04fd5333276ce2cf6
DITSHOOT <dimitri.ch42@gmail.com>
2023-11-16 22:56:05 +0100

    dsfsfs

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 
     prefix: "philo!",
-    //token: "MTE3NDgwNjQ3NDU0Mzg3ODE1NA.GvKYXH.hNljfAbBQkE19_2GpPevzmFfxpB52RYYN6FY6k"
+    ///token: "MTE3NDgwNjQ3NDU0Mzg3ODE1NA.GvKYXH.hNljfAbBQkE19_2GpPevzmFfxpB52RYYN6FY6k"
 }
